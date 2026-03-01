701ae9cb49d704693a28944752391b87c7c1f1c5
Ionuț Bogdan Cristea <65246713+ionutbogdancristea@users.noreply.github.com>
2026-03-01 22:07:35 +0200

    Implement treasury modification on first tick
    Adds a callback to modify the treasury for the local faction on turn 1.

---
+++ unlock_dragonships.lua
@@ -0,0 +1,5 @@
+cm:add_first_tick_callback(function()
+    local faction_key = cm:get_local_faction_name(true)
+    cm:treasury_mod(faction_key, 10000)
+    out("[UNLOCK_DRAGONSHIPS] Script loaded. Added 10000 gold to " .. tostring(faction_key))
+end)
