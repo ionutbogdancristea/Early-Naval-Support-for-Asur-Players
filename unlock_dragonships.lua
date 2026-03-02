dc355375780f65e5dfd01d6aeddec87897a1985e
Ionuț Bogdan Cristea <65246713+ionutbogdancristea@users.noreply.github.com>
2026-03-02 10:05:08 +0200

    Confed Aislinn as Teclis on turn 1
    Removed treasury modification on first tick and added confederation logic for Aislinn faction.

--- unlock_dragonships.lua
+++ unlock_dragonships.lua
@@ -1,5 +1,17 @@
+local PLAYER_FACTION_KEY  = "wh2_main_hef_order_of_loremasters"
+local AISLINN_FACTION_KEY = "wh3_dlc27_hef_aislinn"
+
 cm:add_first_tick_callback(function()
-    local faction_key = cm:get_local_faction_name(true)
-    cm:treasury_mod(faction_key, 10000)
-    out("[UNLOCK_DRAGONSHIPS] Script loaded. Added 10000 gold to " .. tostring(faction_key))
+
+    local local_faction = cm:get_local_faction_name(true)
+    if local_faction ~= PLAYER_FACTION_KEY then
+        return
+    end
+
+    local aislinn = cm:get_faction(AISLINN_FACTION_KEY)
+
+    if aislinn and not aislinn:is_dead() then
+        cm:force_confederation(PLAYER_FACTION_KEY, AISLINN_FACTION_KEY)
+    end
+
 end)
